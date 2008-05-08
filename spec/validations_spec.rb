c38b5272aab88dc79e3e7b443a28c18f4cfb32e8
Kerry Buckley <kerryjbuckley@gmail.com>
2008-05-08 08:42:11 +0100

    Use a new model class for each validation.

--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -1,10 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-class Model < ActiveRecord::Base
-  attr_accessor :name
-  
-  validates_presence_of :name
-  
+class PseudoActiveRecord < ActiveRecord::Base
   # Prevent ActiveRecord initialisation, as we don't have a database
   def initialize; end
   
@@ -17,18 +13,86 @@ class Model < ActiveRecord::Base
       self.send("#{name}=", value) if self.respond_to?("#{name}=")
     end
   end
+  def inspect
+    "#{self.class} #{object_id}"
+  end
 end
 
 describe "'validate_presence_of' matcher" do
+  class PresenceModel < PseudoActiveRecord
+    attr_accessor :name
+    validates_presence_of :name
+  end
+
+  before do
+    @model = PresenceModel.new
+  end
+  
   it "should have the label 'model to validate the presence of <attr>'" do
     validate_presence_of(:foo).description.should == "model to validate the presence of foo"
   end
   
   it "should match for an instance when the validation is present" do
-    Model.new.should validate_presence_of(:name)
+    @model.should validate_presence_of(:name)
   end
   
-  it "should not match for an instance when the association is not present" do
-    Model.new.should_not validate_presence_of(:foo)
+  it "should not match for an instance when the validation is not present" do
+    @model.should_not validate_presence_of(:foo)
+  end
+end
+
+describe "'validate_length_of' matcher" do
+  describe "using 'within'" do
+    class LengthWithinModel < PseudoActiveRecord
+      attr_accessor :name
+      validates_length_of :name, :within => 2...20
+    end
+
+    before do
+      @model = LengthWithinModel.new
+    end
+
+    it "should have the label 'model to validate the length of <attr> within <min> and <max>'" do
+      validate_length_of(:foo, :within => 1...2).description.should == "model to validate the length of foo within 1 and 2"
+    end
+  
+    it "should match for an instance when the validation is present" do
+      @model.should validate_length_of(:name, :within => 2...20)
+    end
+  
+    it "should not match for an instance when the minimum is too low" do
+      @model.should_not validate_length_of(:name, :within => 3...20)
+    end
+  
+    it "should not match for an instance when the minimum is too high" do
+      @model.should_not validate_length_of(:name, :within => 1...20)
+    end
+  
+    it "should not match for an instance when the validation is not present" do
+      @model.should_not validate_length_of(:foo, :within => 1...2)
+    end
+  end
+
+  describe "using 'is'" do
+    class LengthIsModel < PseudoActiveRecord
+      attr_accessor :name
+      validates_length_of :name, :is => 4
+    end
+
+    before do
+      @model = LengthIsModel.new
+    end
+
+    it "should have the label 'model to validate the length of <attr> within <length> and <length>'" do
+      validate_length_of(:foo, :is => 3).description.should == "model to validate the length of foo within 3 and 3"
+    end
+  
+    it "should match for an instance when the validation is present" do
+      @model.should validate_length_of(:name, :is => 4)
+    end
+  
+    it "should not match for an instance when the validation is not present" do
+      @model.should_not validate_length_of(:foo, :is => 4)
+    end
   end
 end
