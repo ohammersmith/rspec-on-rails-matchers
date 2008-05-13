4de7bd6355c95c6ba1a5a26d8ecb8d6ed5ecc6ac
Kerry Buckley <kerryjbuckley@gmail.com>
2008-05-13 08:07:57 +0100

    Started specs for views. Don't really like the way I'm using mocking, but then don't really want to have to include rspec_on_rails either.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,7 @@ Dir.glob(File.dirname(__FILE__) + '/../lib/spec/rails/matchers/*.rb').each do |f
 end
 
 include Spec::Rails::Matchers
+
+# Dir.glob(File.dirname(__FILE__) + '/../rspec_on_rails/**/*.rb').each do |f|
+#   require f
+# end
