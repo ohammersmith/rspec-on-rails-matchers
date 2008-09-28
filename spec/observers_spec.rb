d3fed32dc3c84024a92892f9321e981d803692a6
Carlos Kozuszko <carlos@insignia4u.com>
2008-09-28 10:18:43 -0300

    Fixing 'observe matcher' spec to work with 'fs' fork changes on the description'

--- spec/observers_spec.rb
+++ spec/observers_spec.rb
@@ -9,17 +9,17 @@ end
 
 describe "'observe' matcher" do
   it "should have the label 'observer to be observing <model>'" do
-    observe(Model).description.should == "observer to be observing Model"
+    observe(Model).description.should == "observe Model"
   end
-    
+
   it "should match for an instance which is observing the model" do
     ModelObserver.instance.should observe(Model)
   end
-  
+
   it "should have the failure message 'expected <observer> to observe <model>, but it was not included in [<observed classes>]'" do
     class Foo < ActiveRecord::Base
     end
-    
+
     match = observe(Foo)
     ModelObserver.instance.should match rescue # This is the assertion being tested, and should fail.
     match.failure_message.should == 'expected ModelObserver to observe Foo, but it was not included in [Model]'
