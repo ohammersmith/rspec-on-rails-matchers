46819ffbcae568e0d4ad684200a0cac366362267
Matt Patterson <matt@reprocessed.org>
2008-11-12 14:59:48 +0000

    Fix typo in ControllerActionFacade

--- lib/spec/rails/matchers/controllers.rb
+++ lib/spec/rails/matchers/controllers.rb
@@ -2,11 +2,11 @@ module Spec
   module Rails
     module Matchers
       class ControllerActionFacade
-        attr_reader :action
+        attr_reader :action_name
         
-        def initialize(controller_instance, action)
+        def initialize(controller_instance, action_name)
           @controller_instance = controller_instance
-          @action = action
+          @action_name = action_name
         end
         
         def method_missing(meth, *args)
@@ -14,16 +14,28 @@ module Spec
         end
       end
       
-      def use_before_filter(filter_name, options = {})
+      def use_filter(kind, name, options = {})
         return simple_matcher("controller to use before_filter #{filter_name}") do |controller|
           raise ArgumentError unless options.has_key?(:for)
           # controller = controller.class if controller.is_a? ActionController::Base
           potential_filters = controller.class.filter_chain.select do |filter| 
-            filter.before? && filter.method == filter_name.to_sym
+            filter.send("#{kind.to_s}?".to_sym) && filter.method == name.to_sym
           end
           return false if potential_filters.empty?
           potential_filters.first.send(:should_run_callback?, ControllerActionFacade.new(controller, options[:for]))
         end
+      end
+      
+      def use_before_filter(name, options = {})
+        use_filter(:before, name, options)
+      end
+      
+      def use_after_filter(name, options = {})
+        use_filter(:after, name, options)
+      end
+      
+      def use_around_filter(name, options = {})
+        use_filter(:around, name, options)
       end
     end
   end
